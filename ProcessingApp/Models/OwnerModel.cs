958208b572bb04b44fdc510fe4a2a921fea8918c
KirillSBarsukov <200395896@student.georgianc.on.ca>
2019-11-11 20:37:47 -0500

    Created Property and Owner models

---
+++ ProcessingApp/Models/OwnerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessingApp.Models
+{
+    public class OwnerModel
+    {
+        // Key
+        [Key]
+        public virtual int OwnerId { get; set; }
+        public virtual String OwnerName { get; set; }
+    }
+}
---
+++ ProcessingApp/Models/PropertyModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessingApp.Models
+{
+    public class PropertyModel
+    {
+        // Key
+        [Key]
+        public virtual int PropertyId { get; set; }
+        public virtual String PropertyName { get; set; }
+        public virtual String PropertyAdress { get; set; }
+        public virtual double PropertyPrice { get; set; }
+        public virtual OwnerModel Owner { get; set; }
+
+    }
+}
